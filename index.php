aa948b306f4c5ec175946dfa48515c32243df920
EC2 Default User <ec2-user@ip-172-31-3-222.ap-northeast-1.compute.internal>
2020-12-30 14:22:57 +0000

    掲示板作成の練習（最低限の機能はつけた）

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
     $database = new PDO('mysql:host=localhost;dbname=bbsdatabase;charset=UTF8;','root','');
-
+    
+    $sql = 'SELECT * FROM comments ORDER BY id ASC';
+    $statement = $database->query($sql);
+    $records = $statement->fetchAll();
+    
+    $post_title = $_POST['title'];
+    $post_title = htmlspecialchars($post_title, ENT_QUOTES, 'UTF-8');
+    $post_comment = $_POST['content'];
+    $post_comment = htmlspecialchars($post_comment, ENT_QUOTES, 'UTF-8');
+    
+    
 ?>
 
 <!DOCTYPE html>
@@ -14,21 +24,44 @@
         <h1 class="header-ttl">掲示板</h1>
         <div class="container">
             <div class="posted-wrapper">
-                <!--繰り返す部分-->
-                <div class="posted-message">
-                    <h2><?php  ?>タイトル</h2>
-                    <p class="content"><?php  ?>コメント</p>
-                </div>
-                <!--繰り返す部分ここまで-->
+                
+                <?php
+                foreach($records as $record){
+                    $id = $record['id'];
+                    $title = $record['title'];
+                    $comment = $record['comment'];
+                    
+                    echo 
+                    "<div class=\"posted-message\">
+                        <h2>$id. $title</h2>
+                    <p class=\"content\">$comment</p>
+                    </div>";
+                }
+                
+                
+                ?>
             </div>
         <div class="post-wrapper">
-            <form>
+            <form method="post">
                 <div class="post-input">
                     <label for="title">タイトル</label><input class="input-ttl" type="text" name="title"/>
                     <label for="content">コメント</label><textarea class="input-content" name="content"></textarea>
                 </div>
                 <input class="btn" type="submit" value="投稿" />
             </form>
+            <?php
+            if($post_title && $post_comment){
+                $post_sql = 'insert into comments (title, comment) values (:title, :comment)';
+                $statement = $database->prepare($post_sql);
+                $statement->bindParam(':title', $post_title);
+                $statement->bindParam(':comment', $post_comment);
+                $statement->execute();
+            } elseif(empty($post_title) && empty($post_comment)){
+                echo '';
+            } elseif(!$post_title || !$post_comment) {
+                echo "<p class=\"notice\">タイトルとコメントを入力してください</p>";
+            }
+            ?>
         </div>
         </div>
     </body>
